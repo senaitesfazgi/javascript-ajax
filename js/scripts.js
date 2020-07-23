06462504942092307c48d2b76922b1ce1f4a7594
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-07-23 12:44:35 -0600

    added a fetch response data from a space station website

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
 /**
  * AJAX Examples
  */
-
- //Retrieving an external resource (text file)
- fetch("https://www.w3schools.com/xml/ajax_info.txt")
- //we can use .then() to parse the response
- .then(response =>{
-     console.log(response);
- })
-
   //Retrieving an external resource (live JSON data)
   fetch("http://api.open-notify.org/astros.json")
   //we can use .then() to parse the response
   .then(response =>{
-      console.log(response);
+      return response.json()
   })
+  //Data has been formatted lets have a look inside
+  .then(data => {
+      console.log(data);
+  })
+   
