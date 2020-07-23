c19f912b82f1d0804777b6fd18e2af5d18215794
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-07-23 12:52:30 -0600

    Added condition if a response is successful

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,19 @@
   fetch("http://api.open-notify.org/astros.json")
   //we can use .then() to parse the response
   .then(response =>{
-      return response.json()
+      if (response.status >= 200 && response.status <=299)
+      {
+        return response.json()//convert to JSON and convert the next step(.then())
+      }
+      else
+      {//if it is another range we are unsuccessful
+          throw Error(response.statusText);
+      }
+     
   })
   //Data has been formatted lets have a look inside
   .then(data => {
+      //Out put data to console fot testing
       console.log(data);
   })
    
