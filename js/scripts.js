c98d0bc07ef6634963c74154a282c9e4d01e6320
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-07-23 11:34:26 -0600

    npm server installed to use localhost instead of whole directory

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-alert("hello World");
+/**
+ * AJAX Examples
+ */
+
+ //Retrieving an external resource (text file)
+ fetch("https://www.w3schools.com/xml/ajax_info.txt")
+ //we can use .then() to parse the response
+ .then(response =>{
+     console.log(response);
+ })
+
+  //Retrieving an external resource (live JSON data)
+  fetch("http://api.open-notify.org/astros.json")
+  //we can use .then() to parse the response
+  .then(response =>{
+      console.log(response);
+  })
